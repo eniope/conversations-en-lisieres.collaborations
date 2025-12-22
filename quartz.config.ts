f0754250dec522a65ae1cd129ce4591a2555b24b
Stéphane Caillaud <s.caillaud@gmail.com>
2025-12-22 16:25:47 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Morrowind Modding Wiki",
+    pageTitle: "Conversations en lisieres",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
