357d7f408ab018810d6ae591c58a777c8851f271
Stéphane Caillaud <s.caillaud@gmail.com>
2025-12-23 20:17:50 +0100

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -3,8 +3,6 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
-const iconPath = "/static/Logo.png"
-
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
@@ -12,7 +10,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   return (
     <h2 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>
-        <img class="Logo" src={iconPath} alt={title} />
+        <img class="Logo" src={`${baseDir}static/Logo.png`} alt={title} />
       </a>
     </h2>
   )
@@ -23,16 +21,6 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   text-align: center;
-}
-
-.page-title-link {
-  text-decoration: none;
-  color: inherit;
-}
-
-.page-title-text {
-  display: inline-block;
-  line-height: 1.2;
 }
 `
 
