6314efda99f5fb4c571f716fdcc3b3b14d90e720
Stéphane Caillaud <s.caillaud@gmail.com>
2025-12-25 11:46:41 +0100

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -2,25 +2,23 @@ import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
-import Logo from "./Logo"
-
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
 
-  // 🔑 Chemin correct vers la racine du site (GitHub Pages friendly)
+  // Chemin vers la racine du site (compatible GitHub Pages / Cloudflare)
   const baseDir = pathToRoot(fileData.slug!)
 
   return (
-<h2 class={classNames(displayClass, "page-title")}>
-  <a href={baseDir} class="page-title-link"> 
-    {/* <img class="Logo" 
-    src={`${baseDir === "." ? "./" : baseDir}static/Logo.png`} 
-    alt={title} 
-    />  */}
-    <Logo />
-    </a> 
-</h2>
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir} class="page-title-link">
+        <img
+          class="site-logo"
+          src={`${baseDir === "." ? "./" : baseDir}assets/images/logo.png`}
+          alt={title}
+        />
+      </a>
+    </h2>
   )
 }
 
@@ -34,9 +32,11 @@ PageTitle.css = `
   display: inline-block;
 }
 
-.Logo {
-  max-width: 100%;
+.site-logo {
+  max-width: 420px;
   height: auto;
+  display: block;
+  margin: 2rem auto 1.5rem auto;
 }
 `
 
