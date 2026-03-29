27227a72a7d5792f9ac308facf47b3e76ee69d93
Stéphane Caillaud <s.caillaud@gmail.com>
2026-03-29 19:46:57 +0200

    Update LinksHeader links to new paths

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -3,10 +3,10 @@ import { QuartzComponent, QuartzComponentProps } from "./types"
 const LinksHeader: QuartzComponent = (_props: QuartzComponentProps) => {
   return (
     <nav class="links-header">
-      <a class="links-header-item" href="/conversations-en-lisieres.collaborations/billets/">Billets</a>
-      <a class="links-header-item" href="/conversations-en-lisieres.collaborations/dossiers/">Dossiers</a>
-      <a class="links-header-item" href="/conversations-en-lisieres.collaborations/pages-/a-propos">À propos</a>
-      <a class="links-header-item" href="/conversations-en-lisieres.collaborations/pages-/abonnement">Abonnement</a>
+      <a class="links-header-item" href="/billets/">Billets</a>
+      <a class="links-header-item" href="/dossiers/">Dossiers</a>
+      <a class="links-header-item" href="/pages-/a-propos">À propos</a>
+      <a class="links-header-item" href="/pages-/abonnement">Abonnement</a>
     </nav>
   )
 }
